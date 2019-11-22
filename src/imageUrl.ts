e2c1737a227e29e532888d37b5905c5a42cc711d
KavyaSKumar31 <kavyakumar31@gmail.com>
2019-11-22 11:44:05 +0530

    first commit

---
+++ src/imageUrl.ts
@@ -0,0 +1,18 @@
+export const products = [
+    {
+      url1 : '/src/assets/images/details-1-office-worker.svg'
+    },
+    {
+        url2 : '/src/assets/images/try12.png'
+    },
+    {
+        url3 : 'assets/js/jquery.min.js'
+    },
+    {
+        url4 : 'assets/js/proper.min.js'
+    },
+    {
+        url5 : 'assets/js/scripts.js'
+    }
+  ];
+  
